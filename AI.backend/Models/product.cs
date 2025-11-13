1fc746f1c9c61a64b4011c97669fea78c8cbb69a
JonUkmata <jondukmata@gmail.com>
2025-11-13 20:04:05 +0100

    Update Product model

---
+++ AI.backend/Models/product.cs
@@ -0,0 +1,7 @@
+public class Product
+{
+    public int Id { get; set; }              // Identifikues unik
+    public string Name { get; set; }         // Emri i produktit
+    public decimal Price { get; set; }       // Çmimi
+    public int StockQuantity { get; set; }   // Sasia në stok
+}

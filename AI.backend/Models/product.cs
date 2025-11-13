79a4c1e3f0a799d3ef50dda949c372927faefb4b
Valeza Dobruna <valezadobruna@gmail.com>
2025-11-13 20:11:05 +0100

    t1

--- AI.backend/Models/product.cs
+++ AI.backend/Models/product.cs
@@ -3,5 +3,5 @@ public class Product
     public int Id { get; set; }              // Identifikues unik
     public string Name { get; set; }         // Emri i produktit
     public decimal Price { get; set; }       // Çmimi
-    public int StockQuantity { get; set; }   // Sasia në stok
+    public int StockQuantity { get; set; }   // Sasia në stokut 
 }
